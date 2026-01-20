566e834f4cb278276ec62fba71e4b2ff2a247510
Taha-thiam <taha@groupeisi.com>
2026-01-20 22:38:30 +0000

    Refactor: amélioration de la lisibilité du code et suppression d'espaces inutiles

--- V3/index.php
+++ V3/index.php
@@ -9,40 +9,37 @@ require_once 'bootstrap.php';
 $model = new EtudiantModel($entityManager);
 $controller = new EtudiantController($model);
 
-if(isset($_GET['action']) && !empty($_GET['action'])){
+if (isset($_GET['action']) && !empty($_GET['action'])) {
 
-  if($_GET['action']=="addEtudiant"){
+   if ($_GET['action'] == "addEtudiant") {
       $controller->addFormEtudiant(); //controller
-  }
-  if($_GET['action']=="saveEtudiant"){ 
-     $nom = $_POST['nom'];
-     $prenom = $_POST['prenom'];
-     $email = $_POST['email'];
-     $filiere = $_POST['filiere'];
-     $controller->saveFormEtudiant($nom,$prenom,$email,$filiere);//controller
-  }
-
- if($_GET['action']=="updateEtudiant"){ 
-   extract($_POST);
-   $controller->updateOneEtudiant($id,$nom,$prenom,$email,$filiere);
- }
-
-   if(isset($_GET['id']) && !empty($_GET['id'])){
-      if($_GET['action']=="deleteEtudiant"){
-          $id= $_GET['id'];
-          $controller->deleteOneEtudiant($id); // controller
-      }
-
-       if($_GET['action']=="editEtudiant"){
-          $id= $_GET['id'];
-          $controller->editEtudiant($id); //controller
-       }
+   }
+   if ($_GET['action'] == "saveEtudiant") {
+      $nom = $_POST['nom'];
+      $prenom = $_POST['prenom'];
+      $email = $_POST['email'];
+      $filiere = $_POST['filiere'];
+      $controller->saveFormEtudiant($nom, $prenom, $email, $filiere); //controller
    }
 
-}else{
- $controller->listEtudiant(); //controller
+   if ($_GET['action'] == "updateEtudiant") {
+      extract($_POST);
+      $controller->updateOneEtudiant($id, $nom, $prenom, $email, $filiere);
+   }
+
+   if (isset($_GET['id']) && !empty($_GET['id'])) {
+      if ($_GET['action'] == "deleteEtudiant") {
+         $id = $_GET['id'];
+         $controller->deleteOneEtudiant($id); // controller
+      }
+
+      if ($_GET['action'] == "editEtudiant") {
+         $id = $_GET['id'];
+         $controller->editEtudiant($id); //controller
+      }
+   }
+} else {
+   $controller->listEtudiant(); //controller
 }
 
-
-
-?> 
+?>
